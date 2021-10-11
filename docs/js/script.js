a6c7f6085de178769483335d75916c208cbddfc6
Leweyse <darylcastro67@gmail.com>
2021-10-11 12:15:39 +0200

    added new functions

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,39 +1,85 @@
 const run = document.getElementById('run');
 const counter = document.querySelector('.counter');
 const multiplier = document.getElementById('multiplier');
+const autoclick = document.getElementById('autoclick');
+const timer = document.getElementById('timer');
 
 let count = 0;
 let increment = 1;
 let multiplierIndex = 2;
-let price = 100;
+let multiplierPrice = 100;
+
+let intervalAC = null;
+let intervalT = null;
 
 multiplier.style.display = 'none';
+autoclick.style.display = 'none';
+timer.style.display = 'none';
 
 run.addEventListener('click', function(){
     count += increment;
     counter.innerHTML = count;
+    
+    if (count >= ((multiplierPrice * 3) / 4)) {
+        timer.style.display = "block";
 
-    if (count >= price) {
-        multiplier.style.display = 'block';
+        if (count >= multiplierPrice) {
+            multiplier.style.display = 'block';
+        }
     }
 });
 
 multiplier.addEventListener('click', function(){
     increment *= multiplierIndex;
 
-    if (count >= price) {
-        count -= price;
+    if (count >= multiplierPrice) {
+        count -= multiplierPrice;
     }
 
     counter.innerHTML = count;
 
-    price *= multiplierIndex;
+    multiplierPrice *= multiplierIndex;
 
-    if (count < price) {
-        multiplier.style.display = 'none';
+    if (count < ((multiplierPrice * 3) / 4)) {
+        timer.style.display = "none";
+
+        if (count < multiplierPrice) {
+            multiplier.style.display = 'none';
+        }
     }
 
     multiplierIndex += 1;
 
-    multiplier.innerHTML = `${price} points for multiply by ${multiplierIndex}`
+    multiplier.innerHTML = `${multiplierPrice} points for multiply by ${multiplierIndex}`
+
+    if (multiplierIndex === 5) {
+        autoclick.style.display = "block";
+    }
 });
+
+autoclick.addEventListener('click', function(){
+    clearInterval(intervalAC);
+    intervalAC = setInterval(() => {
+        run.click();
+    }, 10000);
+    autoclick.style.display = "none";
+})
+
+timer.addEventListener('click', function(){
+    let timeRest = 29;
+
+    increment *= 2;
+
+    intervalT = setInterval(() => {
+        timer.style.display = 'block';
+        timer.innerHTML = `${timeRest} seconds`;
+        timeRest -= 1;
+    }, 1000);
+
+    setTimeout(() => {
+        clearInterval(intervalT);
+        timer.style.display = 'none';
+        increment /= 2;
+        timer.innerHTML = `30 seconds`;
+    }, 30000);
+})
