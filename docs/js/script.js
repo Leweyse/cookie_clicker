cd659fa18120181e1d7dcab2f899d4bab8de585a
Leweyse <darylcastro67@gmail.com>
2021-10-11 10:27:31 +0200

    added multiplier event

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,14 @@
 let count = 0;
+let increment = 1;
 
 document.getElementById('run').addEventListener('click', function(){
-    count += 1;
+    count += increment;
     console.log(count);
     document.querySelector('.counter').innerHTML = count;
-})
+});
+
+document.getElementById('multiplier').addEventListener('click', function(){
+    increment *= 2;
+    console.log(count);
+    document.querySelector('.counter').innerHTML = count;
+});
